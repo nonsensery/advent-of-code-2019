f49047c7e8c96877e6bc8c4398c8a1d200034a92
Alex Johnson <alex27@mac.com>
2019-12-02 13:14:45 -0800

    Revise solutions for Day 2, JFF

--- 02. 1202 Program Alarm.playground/Contents.swift
+++ 02. 1202 Program Alarm.playground/Contents.swift
@@ -1,18 +1,10 @@
 import Foundation
 
-let program = Bundle.main.url(forResource: "gravity-assist", withExtension: "intcode")
-    .flatMap({ try? Data(contentsOf: $0) })
-    .flatMap({ String(data: $0, encoding: .utf8) })
-    .map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })?
-    .split(separator: ",")
-    .map({ Int($0)! })
-    ?? []
+typealias MemoryValue = Int
+typealias Memory = [MemoryValue]
+typealias MemoryAddress = Memory.Index
 
 struct Computer {
-    typealias MemoryValue = Int
-    typealias Memory = [MemoryValue]
-    typealias MemoryAddress = Memory.Index
-
     enum Instruction {
         case math((MemoryValue, MemoryValue) -> MemoryValue, lhs: MemoryAddress, rhs: MemoryAddress, dest: MemoryAddress)
         case halt
@@ -65,18 +57,25 @@ struct Computer {
     }
 }
 
-func compute(noun: Int, verb: Int) -> Int {
+func compute(program: Memory, noun: MemoryValue, verb: MemoryValue) -> MemoryValue {
     var computer = Computer(memory: program)
-
     computer.memory[1...2] = [noun, verb]
     computer.run()
 
     return computer.memory[0]
 }
 
+let program = Bundle.main.url(forResource: "gravity-assist", withExtension: "intcode")
+    .flatMap({ try? Data(contentsOf: $0) })
+    .flatMap({ String(data: $0, encoding: .utf8) })
+    .map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })?
+    .split(separator: ",")
+    .map({ MemoryValue($0)! })
+    ?? []
+
 // MARK: Part 1
 
-print(compute(noun: 12, verb: 02))
+print(compute(program: program, noun: 12, verb: 02))
 //12490719
 
 // MARK: Part 2
@@ -85,8 +84,8 @@ let desiredOutput = 19690720
 
 // This is based on some observed behavior of how the noun and verb affect the output.
 // Not sure how to generalize it without solving the halting problem...
-let noun = (0...99).first(where: { compute(noun: $0 + 1, verb: 0) > desiredOutput })!
-let verb = (0...99).first(where: { compute(noun: noun, verb: $0) == desiredOutput })!
+let noun = (0...99).first(where: { compute(program: program, noun: $0 + 1, verb: 0) > desiredOutput })!
+let verb = (0...99).first(where: { compute(program: program, noun: noun, verb: $0) == desiredOutput })!
 
 print("\(noun * 100 + verb)")
 //203
