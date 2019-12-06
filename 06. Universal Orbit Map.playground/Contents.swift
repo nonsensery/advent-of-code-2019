9150f0e62b520f7fd8f8e6cae9aa2e6adca13ae9
Alex Johnson <alex27@mac.com>
2019-12-06 12:04:12 -0800

    [D6] [P2] solution

--- 06. Universal Orbit Map.playground/Contents.swift
+++ 06. Universal Orbit Map.playground/Contents.swift
@@ -35,3 +35,15 @@ func countOrbits(mappings: [String: String]) -> Int {
 
 print(countOrbits(mappings: Resource.sample1_txt.mappings)) // => 42
 print(countOrbits(mappings: Resource.input_txt.mappings)) // => 322508
+
+// MARK: - Part 2
+
+func minOrbitalTransfers(from a: String, to b: String, mappings: [String: String]) -> Int {
+    let pathA = sequence(first: a, next: { mappings[$0] }).dropFirst()
+    let pathB = sequence(first: b, next: { mappings[$0] }).dropFirst()
+
+    return Set(pathA).symmetricDifference(pathB).count
+}
+
+print(minOrbitalTransfers(from: "YOU", to: "SAN", mappings: Resource.sample2_txt.mappings)) // => 4
+print(minOrbitalTransfers(from: "YOU", to: "SAN", mappings: Resource.input_txt.mappings)) // => 496
